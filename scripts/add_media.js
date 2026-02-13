485ce065b75acf4dac2ea6fcec676f182271dcc5
Yanis <Yanis.BOukhedra06013@gmail.com>
2026-02-13 17:28:14 +0100

    nettoyage de l'architecture

--- scripts/add_media.js
+++
@@ -1,9 +0,0 @@
-const filePicker = document.getElementById('file-picker');
-const form = document.getElementById('add-media');
-
-filePicker.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-        form.submit();
-    }
-});
--- scripts/scroll_to_closest_event.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const closestEvent = document.getElementById('closest-event');
-    if (closestEvent) {
-        closestEvent.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-});
